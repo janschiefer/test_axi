cd6270dd72110aa082c6c12294ab046bad9ad75a
Jan Schiefer <janschiefer@users.noreply.github.com>
2025-04-08 01:52:13 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
-source ~/.sdkman/bin/sdkman-init.sh
 source ~/oss-cad-suite/environment
 veryl clean
 veryl build
 echo "Yosys..."
-yosys -l yosys.log -p "read_verilog -sv output/*.sv; synth_gowin -top axi4_test -json build.yosys.json -family gw2a" 
+yosys -l yosys.log -p "read_verilog -sv output/axi_if.sv output/axi_test.sv; synth_gowin -top axi_test -json build.yosys.json -family gw2a" 
+echo "Yosys variant 2..."
+yosys -l yosys-variant.log -p "read_verilog -sv output/axi_if_variant.sv output/axi_test_variant.sv; synth_gowin -top axi_test -json build.yosys.json -family gw2a" 
 
-#nextpnr-himbaechel --log nextpnr.log --detailed-timing-report --freq 83 --timing-allow-fail --device GW2AR-LV18QN88C8/I7 --vopt family=GW2A-18C --vopt cst=tangnano20k.cst  --json build.yosys.json --write build.nextpnr.json
