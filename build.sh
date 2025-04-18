527c6c53d9a25286597a67b91126b74503e4378f
Jan Schiefer <janschiefer@users.noreply.github.com>
2025-04-18 21:43:25 +0200

    Remove ACLK ARESET, use slang yosys plugin

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ source ~/oss-cad-suite/environment
 veryl clean
 veryl build
 echo "Yosys..."
-yosys -l yosys.log -p "read_verilog -sv output/axi_if.sv output/axi_test.sv; synth_gowin -top axi_test -json build.yosys.json -family gw2a" 
+yosys -m slang -l yosys.log -p "read_slang output/src/axi_if.sv output/src/axi_test.sv; synth_gowin -top axi_test -json build.yosys.json -family gw2a" 
 echo "Yosys variant 2..."
-yosys -l yosys-variant.log -p "read_verilog -sv output/axi_if_variant.sv output/axi_test_variant.sv; synth_gowin -top axi_test -json build.yosys.json -family gw2a" 
+yosys -m slang  -l yosys-variant.log -p "read_slang output/src/axi_if_variant.sv output/src/axi_test_variant.sv; synth_gowin -top axi_test_variant -json build.yosys.json -family gw2a" 
 
