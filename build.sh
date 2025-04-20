1a7ed88b03cadc287125b3145c8566947c977918
Jan Schiefer <janschiefer@users.noreply.github.com>
2025-04-20 15:56:01 +0200

    axi3/4 prototypes

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ veryl clean
 veryl build
 echo "Yosys..."
 yosys -m slang -l yosys.log -p "read_slang output/src/axi_if.sv output/src/axi_test.sv; synth_gowin -top axi_test -json build.yosys.json -family gw2a" 
-echo "Yosys variant 2..."
-yosys -m slang  -l yosys-variant.log -p "read_slang output/src/axi_if_variant.sv output/src/axi_test_variant.sv; synth_gowin -top axi_test_variant -json build.yosys.json -family gw2a" 
 
