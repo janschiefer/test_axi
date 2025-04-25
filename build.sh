d8d234dd9ef118268003de68c173f94ff3e9e607
Jan Schiefer <janschiefer@users.noreply.github.com>
2025-04-25 16:03:59 +0200

    Unify AXI4, AXI3 and AXI4-Lite interfaces, update test and fpga build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ source ~/oss-cad-suite/environment
 veryl clean
 veryl build
 echo "Yosys..."
-yosys -m slang -l yosys.log -p "read_slang output/src/axi_if.sv output/src/axi_test.sv; synth_gowin -top top -json build.yosys.json -family gw2a" 
+yosys -m slang -l yosys.log -p "read_slang output/src/*.sv output/src/*.sv; synth_gowin -top top -json build.yosys.json -family gw2a" 
 
